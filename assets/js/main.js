3212d4993d426a2b358346d384cc482d08af474d
Nkumah7 <67166961+Nkumah7@users.noreply.github.com>
2021-04-07 11:33:28 +0100

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,33 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.nav-menu').addClass("sticky");
+    }else {
+      $('.nav-menu').removeClass("sticky");
+    }
+    if(this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    }else {
+      $('.scroll-up-btn').removeClass("show");
+    }    
+  });
 
-(function() {
-  "use strict";
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0})
+  });
+
+   // typing animation script
+   var typed = new Typed(".typing", {
+    strings: ["Developers", "Designers", "Digital Marketers", "Product Managers"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });  
+})
+
+!(function() {
+  "use strict";  
 
   /**
    * Easy selector helper function
@@ -279,5 +306,8 @@
   window.addEventListener('load', () => {
     aos_init();
   });
-
 })();
+
+
+
+ 
